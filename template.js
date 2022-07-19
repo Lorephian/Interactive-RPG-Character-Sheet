2878deaff7df2973ccffcadb76a99b46c52c92a7
lorephian <guilherme.thomas87@gmail.com>
2022-07-19 14:59:34 -0300

    modified something

---
+++ template.js
@@ -0,0 +1,25 @@
+const sectionId = ["header", "attributes", "abilities", "advantages", "footer"]
+
+const sectionColumn = ["left", "mid", "right"]
+
+const headerLabels = ["Name:", "Player:", "Chronicle:", "Nature:", "Demeanor:", "Concept:", "Clan:", "Generation:", "Sire:"]
+
+const attributes = {
+    Physical: ["Strength", "Dexterity", "Stamina"],
+    Social: ["Charisma", "Manipulation", "Appearance"],
+    Mental: ["Perception", "Intelligence", "Wits"]
+}
+
+const abilities = {
+    Talents: ["Alertness", "Athletics", "Awareness", "Brawl", "Empathy", "Expression", "Intimidation", "Leadership", "Streetwise", "Subterfuge"],
+    Skills: ["Animal Ken", "Crafts", "Drive", "Etiquete", "Firearms", "Larceny", "Melee", "Performance", "Stealth", "Survival"],
+    Knowledges: ["Academics", "Computer", "Finance", "Investigation", "Law", "Medicine", "Occult", "Politics", "Science", "Technology"]
+
+}
+
+const virtues = {
+    1: ["Conscience", "Conviction"],
+    2: ["Self-Control", "Instinct"],
+    3: ["Courage"]
+
+}
